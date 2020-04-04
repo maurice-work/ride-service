ba6f62269c1d1d162a769c6f4a1d130d4bfb9048
maurice-work <maurice.devwork@gmail.com>
2020-04-04 08:25:32 +0200

    add fonts onloadingdone check

--- src/components/Theming/Theming.tsx
+++ src/components/Theming/Theming.tsx
@@ -3,7 +3,9 @@ import { theme } from './Theming.styles';
 import React from 'react';
 
 export const Theming: React.FunctionComponent = ({ children }) => {
-	(window.document as any).fonts.onloadingdone = () => console.log('Fonts are initialized.');
+	if (document.fonts?.onloadingdone) {
+		document.fonts.onloadingdone = () => console.log('Fonts are initialized.');
+	}
 
 	require('./variables.css');
 
