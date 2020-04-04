7ae7a63d7c33afae45a4e7dc3ca91535849f9f96
maurice-work <maurice.devwork@gmail.com>
2020-04-04 12:03:19 +0200

    removed ionic router

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,12 @@
 import './App.styles';
 import { CssBaseline } from '@material-ui/core';
-import { IonApp, IonRouterOutlet } from '@ionic/react';
-import { IonReactRouter } from '@ionic/react-router';
-import { Redirect, Route } from 'react-router-dom';
-import { Splash } from 'pages';
+import { IonApp } from '@ionic/react';
 import { Theming } from 'components';
 import React from 'react';
 
 export const App: React.FunctionComponent = () => (
 	<Theming>
 		<CssBaseline />
-		<IonApp>
-			<IonReactRouter>
-				<IonRouterOutlet>
-					<Route path="/splash" exact component={Splash} />
-					<Route exact path="/" render={() => <Redirect to="/splash" />} />
-				</IonRouterOutlet>
-			</IonReactRouter>
-		</IonApp>
+		<IonApp></IonApp>
 	</Theming>
 );
