c271e83b649ebd68fd330b7561093cf23aff5672
maurice-work <maurice.devwork@gmail.com>
2020-04-10 19:54:10 +0200

    link color updated

--- src/components/Link/Link.styles.ts
+++ src/components/Link/Link.styles.ts
@@ -1,11 +1,27 @@
-import { createTextStyles } from 'components/Text';
+import { CSSProperties } from '@material-ui/styles';
+import { createStyles } from '@material-ui/core';
+import { font } from 'styles';
 
-export const linkText = createTextStyles({
-	fontSize: 15,
-	fontWeight: 600,
-	fontStretch: 'normal',
-	fontStyle: 'normal',
-	lineHeight: 1.67,
-	letterSpacing: 'normal',
-	color: '#00b559'
+const link: CSSProperties = {
+	cursor: 'pointer',
+	textDecoration: 'none',
+	...font({
+		fontSize: 15,
+		fontWeight: 600,
+		fontStretch: 'normal',
+		fontStyle: 'normal',
+		lineHeight: 1.67,
+		letterSpacing: 'normal',
+		color: '#00b559'
+	}),
+	'&:hover': {
+		color: '#009c4c'
+	},
+	'&:active': {
+		color: '#008240'
+	}
+};
+
+export const styles = createStyles({
+	link
 });
--- src/components/Link/Link.tsx
+++ src/components/Link/Link.tsx
@@ -1,9 +1,14 @@
 import { ILinkProps } from './Link.types';
 import { Text } from 'components';
-import { linkText } from './Link.styles';
+import { makeStyles } from '@material-ui/core';
+import { styles } from './Link.styles';
 import React from 'react';
 
+const useStyles = makeStyles(styles);
+
 export const Link: React.FunctionComponent<ILinkProps> = ({ href, onClick, children }) => {
+	const classes = useStyles();
+
 	if (onClick) {
 		onClick = e => {
 			e.preventDefault();
@@ -14,13 +19,13 @@ export const Link: React.FunctionComponent<ILinkProps> = ({ href, onClick, child
 
 	return (
 		<Text
+			className={classes.link}
 			component="a"
 			componentProps={{
 				href,
 				onClick
 			}}
 			inheritStyles
-			textStyles={linkText}
 		>
 			{children}
 		</Text>
