4dc8d0171db383a0a0b4d447b3167e9f2cffbf6f
maurice-work <maurice.devwork@gmail.com>
2020-04-10 15:19:14 +0200

    created basic news page setup

--- src/pages/News/News.styles.ts
+++ src/pages/News/News.styles.ts
@@ -1,3 +1,65 @@
+import { CSSProperties } from '@material-ui/styles';
 import { createStyles } from '@material-ui/core';
+import { font, pxToRem } from 'styles';
 
-export const styles = createStyles({});
+const articleList: CSSProperties = {
+	paddingTop: pxToRem(15)
+};
+
+const articleTitle: CSSProperties = {
+	marginBottom: pxToRem(5),
+	...font({
+		fontSize: 15,
+		fontWeight: 600,
+		fontStretch: 'normal',
+		fontStyle: 'normal',
+		lineHeight: 1.67,
+		letterSpacing: 'normal',
+		color: '#181c19'
+	})
+};
+
+const articleText: CSSProperties = {
+	...font({
+		fontSize: 10,
+		fontWeight: 600,
+		fontStretch: 'normal',
+		fontStyle: 'normal',
+		lineHeight: 1.5,
+		letterSpacing: 'normal',
+		color: 'rgba(24, 28, 25, 0.5)'
+	})
+};
+
+const articleDescription: CSSProperties = {
+	display: 'flex !important',
+	justifyContent: 'space-between',
+	flexDirection: 'row',
+	marginBottom: pxToRem(5),
+	...font({
+		fontSize: 10,
+		fontWeight: 600,
+		fontStretch: 'normal',
+		fontStyle: 'normal',
+		lineHeight: 1.5,
+		letterSpacing: 'normal',
+		color: 'rgba(24, 28, 25, 0.5)'
+	})
+};
+
+const articleTime: CSSProperties = {
+	whiteSpace: 'nowrap'
+};
+
+const platformText: CSSProperties = {
+	color: '#00b559'
+};
+
+export const styles = createStyles({
+	articleList,
+	articleTitle,
+	articleText,
+	articleDescription,
+	articleTime,
+	platformText
+});
--- src/pages/News/News.tsx
+++ src/pages/News/News.tsx
@@ -1,5 +1,31 @@
-import { Page } from 'components';
+import { List, ListItemText } from '@material-ui/core';
+import { Page, Text } from 'components';
+import { makeStyles } from '@material-ui/styles';
 import { styles } from './News.styles';
 import React from 'react';
 
-export const News: React.FunctionComponent = () => <Page title="News" titleSize="large"></Page>;
+const useStyles = makeStyles(styles);
+
+export const News: React.FunctionComponent = () => {
+	const classes = useStyles();
+
+	return (
+		<Page title="News" titleSize="large">
+			<List className={classes.articleList}>
+				<ListItemText>
+					<Text className={classes.articleDescription} block>
+						<div className={classes.platformText}>Facebook</div>
+						<div className={classes.articleTime}>2h ago</div>
+					</Text>
+					<Text className={classes.articleTitle} block>
+						I have unlocked the scooter. How can I start the ride?
+					</Text>
+					<Text className={classes.articleText} block>
+						To start your scooter ride, place one foot firmly on the baseboard and use your other foot to push off against the ground a few
+						times....
+					</Text>
+				</ListItemText>
+			</List>
+		</Page>
+	);
+};
