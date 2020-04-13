72458638b8ff05ae1a33955faac987a0caed3721
maurice-work <maurice.devwork@gmail.com>
2020-04-13 10:55:55 +0100

    fix style of get help

--- src/pages/GetHelp/GetHelp.tsx
+++ src/pages/GetHelp/GetHelp.tsx
@@ -1,36 +1,61 @@
+import { Divider, Button, makeStyles } from '@material-ui/core';
+import { useHistory } from 'react-router-dom';
 import { Page, SidebarMenuItem } from 'components';
 import { Stack } from '@fluentui/react';
+import { styles } from './GetHelp.styles';
 
 import React from 'react';
 
+const useStyles = makeStyles(styles);
+
+const items = [
+  {
+    title: 'Safety regulations',
+    href: '/gethelp/regulations'
+  },
+  {
+    title: 'How to ride',
+    href: '/gethelp/ride'
+  },
+  {
+    title: 'FAQ',
+    href: '/gethelp/faq'
+  },
+  {
+    title: 'Contact us',
+    href: '/gethelp/contact'
+  },
+  {
+    title: 'Add report',
+    href: '/gethelp/add-report'
+  },
+  {
+    title: 'My reports',
+    href: '/gethelp/my-reports'
+  },
+  {
+    title: 'Privacy Policy',
+    href: '/gethelp/privacy-policy'
+  }
+]
 export const GetHelp: React.FunctionComponent = () => {
 
+  const classes = useStyles();
+  const history = useHistory();
 	return (
 		<>
 			<Page title="Get help" titleSize="large">
-				<Stack horizontalAlign="stretch" grow={1} tokens={{ childrenGap: 0 }} verticalFill>
-					<Stack.Item>
-						<SidebarMenuItem title="Safety regulations" href="/settings/profile" />
-					</Stack.Item>
-					<Stack.Item>
-						<SidebarMenuItem title="How to ride" href="/settings/notifications" />
-					</Stack.Item>
-					<Stack.Item>
-						<SidebarMenuItem title="FAQ" href="/gethelp/faq" />
-					</Stack.Item>
-					<Stack.Item>
-						<SidebarMenuItem title="Contact us" href="/settings/languages" />
-					</Stack.Item>
-					<Stack.Item>
-						<SidebarMenuItem title="Add report" href="/settings/change-password" />
-          </Stack.Item>
-          <Stack.Item>
-						<SidebarMenuItem title="My reports" href="/settings/change-password" />
-          </Stack.Item>
-          <Stack.Item>
-						<SidebarMenuItem title="Privacy Policy" href="/settings/change-password" />
-					</Stack.Item>
-				</Stack>
+        <Stack horizontalAlign="stretch" grow={1} tokens={{ childrenGap: 0 }} verticalFill>
+          {
+            items.map((item, index) => (
+              <Stack.Item key={index}>
+                <SidebarMenuItem className={classes.menuItem} title={item.title} href={item.href} />
+                <Divider />
+              </Stack.Item>
+            ))
+          }
+        </Stack>
+        <Button className={classes.bigButton} onClick={() => history.goBack()}>Back</Button>
 			</Page>
 		</>
 	);
