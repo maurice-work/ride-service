892ccd293cd898531216b4fc4aaf5671fe35a1ec
maurice-work <maurice.devwork@gmail.com>
2020-04-16 11:52:45 +0100

    fix my report

--- src/components/GetHelp/pages/MyReports/MyReports.styles.ts
+++ src/components/GetHelp/pages/MyReports/MyReports.styles.ts
@@ -5,7 +5,9 @@ const myReportContainer: CSSProperties = {
 	height: `calc(100vh - ${pxToRem(145)})`,
 	overflow: 'auto',
 	borderBottomLeftRadius: pxToRem(15),
-	borderBottomRightRadius: pxToRem(15)
+	borderBottomRightRadius: pxToRem(15),
+	margin: ` ${pxToRem(-50)} ${pxToRem(-20)} 0 ${pxToRem(-20)}`,
+	padding: ` ${pxToRem(50)} ${pxToRem(20)} 0 ${pxToRem(20)}`
 };
 
 export const styles = createStyles({
