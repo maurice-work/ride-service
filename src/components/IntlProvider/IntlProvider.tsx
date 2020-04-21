44f02fabd9ce1f47ecae3c1438b73c6f927ba83e
maurice-work <maurice.devwork@gmail.com>
2020-04-21 10:21:59 +0800

    extract useLanguage hook from IntlProvider

--- src/components/IntlProvider/IntlProvider.tsx
+++ src/components/IntlProvider/IntlProvider.tsx
@@ -1,15 +1,9 @@
 /* eslint-disable @typescript-eslint/unbound-method */
 import * as React from 'react';
-import { ILanguageContext } from './IntlProvider.types';
 import { RawIntlProvider, createIntl, createIntlCache } from 'react-intl';
 import { getMessages } from './messages';
 
-const initial: ILanguageContext = {
-	locale: navigator.language,
-	changeLanguage: () => null
-};
-
-export const LanguageContext = React.createContext<ILanguageContext>(initial);
+import { LanguageContext, initial } from './LanguageContext';
 
 const cache = createIntlCache();
 /** You can use this variable in other files even after reassigning it. */
@@ -35,8 +29,4 @@ export const IntlProvider: React.FC<{ children: React.ReactNode }> = props => {
 			<RawIntlProvider value={intl}>{children}</RawIntlProvider>
 		</LanguageContext.Provider>
 	);
-};
-
-export const useLanguageContext = (): ILanguageContext => {
-	return React.useContext(LanguageContext);
 };
--- src/components/IntlProvider/index.ts
+++ src/components/IntlProvider/index.ts
@@ -1,1 +1,2 @@
 export * from './IntlProvider';
+export * from './LanguageContext';
