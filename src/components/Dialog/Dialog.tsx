c5e6f5e5d72ee0786dc195ef8b440b9d13702edd
maurice-work <maurice.devwork@gmail.com>
2020-04-23 13:54:33 +0100

    add illustrationName on Dialog component

--- src/components/Dialog/Dialog.tsx
+++ src/components/Dialog/Dialog.tsx
@@ -3,13 +3,14 @@ import { IDialogProps } from './Dialog.types';
 import { IconButton, Image, Text } from 'components';
 import { makeStyles } from '@material-ui/styles';
 import { styles } from './Dialog.styles';
-import React from 'react';
+import React, { useEffect } from 'react';
 
 const useStyles = makeStyles(styles);
 
 export const Dialog: React.FunctionComponent<IDialogProps> = ({
 	title,
 	image,
+	illustrationName,
 	open,
 	hasClose,
 	onClose,
@@ -18,6 +19,19 @@ export const Dialog: React.FunctionComponent<IDialogProps> = ({
 	...restProps
 }) => {
 	const classes = useStyles();
+	const [imagePath, setImagePath] = React.useState(image);
+
+	useEffect(() => {
+		if (illustrationName) {
+			import(`./svg/${illustrationName}.svg`)
+				.then(({ default: iconPath }) => {
+					if (iconPath) {
+						setImagePath(iconPath);
+					}
+				})
+				.catch(() => console.warn(`No icon with the name \`${illustrationName}\` (\`${illustrationName}.svg\`) was found.`));
+		}
+	}, [illustrationName]);
 
 	const handleDeleteAccountClose = (e: React.MouseEvent<HTMLElement, MouseEvent>): void => {
 		onClose && onClose(e, 'escapeKeyDown');
@@ -25,7 +39,7 @@ export const Dialog: React.FunctionComponent<IDialogProps> = ({
 
 	return (
 		<MuiDialog open={open} onClose={handleDeleteAccountClose} classes={{ paper: classes.modalPaper }}>
-			<Box className={classes.imgWrapper}>{image && <Image src={image} />}</Box>
+			<Box className={classes.imgWrapper}>{imagePath && <Image src={imagePath} />}</Box>
 			<DialogTitle classes={{ root: classes.dialogTitle }}>
 				<Text color="primary" className={classes.titleTextStyle} block>
 					{title}
