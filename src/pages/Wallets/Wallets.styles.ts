1fd947a9ee6755621db65f01c550e9225eb4fa20
maurice-work <maurice.devwork@gmail.com>
2020-04-27 16:19:03 +0100

    add wallets page

---
+++ src/pages/Wallets/Wallets.styles.ts
@@ -0,0 +1,152 @@
+import { CSSProperties } from '@material-ui/styles';
+import { createStyles } from '@material-ui/core';
+import { font, pxToRem } from 'styles';
+import walletsLogoImage from './images/card-img.svg';
+
+const walletsLogoContainer: CSSProperties = {
+	padding: `${pxToRem(15)} ${pxToRem(25)} ${pxToRem(30)} ${pxToRem(25)}`,
+	display: 'flex',
+	flexDirection: 'column',
+	alignItems: 'center'
+};
+
+const walletsLogo: CSSProperties = {
+	backgroundImage: `url(${walletsLogoImage})`,
+	width: pxToRem(275),
+	height: pxToRem(82),
+	position: 'relative',
+	padding: `${pxToRem(25)}`,
+	display: 'flex',
+	flexDirection: 'column'
+};
+
+const trashIcon: CSSProperties = {
+	position: 'absolute',
+	top: pxToRem(5),
+	right: pxToRem(5),
+	backgroundColor: 'transparent',
+	padding: 0,
+	boxShadow: 'none',
+	'&:hover': {
+		backgroundColor: 'transparent',
+		opacity: 0.7
+	}
+};
+
+const rulerPriceText: CSSProperties = {
+	...font({
+		fontWeight: 600,
+		lineHeight: 1.5,
+		color: 'rgba(255, 255, 255, 0.5)'
+	}),
+	fontSize: pxToRem(10),
+	paddingTop: pxToRem(12)
+};
+
+const rulerWalletText: CSSProperties = {
+	...font({
+		fontWeight: 600,
+		lineHeight: 1.67,
+		color: '#ffffff'
+	}),
+	fontSize: pxToRem(15)
+};
+
+const rulerNumberText: CSSProperties = {
+	...font({
+		fontWeight: 600,
+		lineHeight: 1.5,
+		color: '#ffffff'
+	}),
+	fontSize: pxToRem(20)
+};
+
+const paymentTemplateContainer: CSSProperties = {
+	paddingLeft: pxToRem(25)
+};
+
+const TemplateButtonsWrapper: CSSProperties = {
+	padding: `${pxToRem(10)} 0 0 0`,
+	display: 'flex',
+	flexDirection: 'row',
+	alignItems: 'center'
+};
+
+const addFabButton: CSSProperties = {
+	width: pxToRem(50),
+	height: pxToRem(50),
+	backgroundColor: '#caf1dd',
+	boxShadow: 'none',
+	minWidth: pxToRem(50)
+};
+
+const paymentTemplateButton: CSSProperties = {
+	backgroundColor: 'transparent',
+	color: '#00b559',
+	borderRadius: pxToRem(100),
+	border: `solid ${pxToRem(2)} #f3f3f3`,
+	padding: `${pxToRem(10)} ${pxToRem(20)} ${pxToRem(10)} ${pxToRem(25)}`,
+	marginLeft: pxToRem(10),
+	fontWeight: 'bold',
+	'&.to-friend': {
+		minWidth: pxToRem(183)
+	},
+	'&.to-wife': {
+		minWidth: pxToRem(140)
+	},
+	'& .MuiButton-label> span': {
+		whiteSpace: 'nowrap'
+	}
+};
+
+const paymentTemplateText: CSSProperties = {
+	...font({
+		fontWeight: 600,
+		lineHeight: 1.5,
+		color: 'rgba(24, 28, 25, 0.5)'
+	}),
+	fontSize: pxToRem(10)
+};
+
+const footer: CSSProperties = {
+	marginTop: 'auto',
+	padding: `${pxToRem(20)} ${pxToRem(20)} 0 ${pxToRem(20)}`,
+	'& button': {
+		fontWeight: 'bold'
+	}
+};
+
+const buttonWrapper: CSSProperties = {
+	display: 'flex',
+	flexDirection: 'row',
+	paddingBottom: pxToRem(15),
+	'& button:first-child': {
+		marginRight: pxToRem(15)
+	}
+};
+
+const dialogContentText: CSSProperties = {
+	fontSize: pxToRem(10),
+	fontWeight: 600,
+	lineHeight: 1.5,
+	color: '#181c19',
+	opacity: 0.5,
+	padding: `${pxToRem(10)} ${pxToRem(10)} ${pxToRem(5)} ${pxToRem(10)}`
+};
+
+export const styles = createStyles({
+	walletsLogoContainer,
+	walletsLogo,
+	rulerWalletText,
+	rulerPriceText,
+	rulerNumberText,
+	trashIcon,
+	paymentTemplateContainer,
+	TemplateButtonsWrapper,
+	paymentTemplateButton,
+	addFabButton,
+	paymentTemplateText,
+	footer,
+	buttonWrapper,
+	dialogContentText
+});
---
+++ src/pages/Wallets/Wallets.tsx
@@ -0,0 +1,94 @@
+import { Box, makeStyles } from '@material-ui/core';
+import { Button, Dialog, GreenButton, Icon, IconButton, LightGreenButton, Page, Text } from 'components';
+import { IWalletsProps } from './Wallets.types';
+import { styles } from './Wallets.styles';
+import { useHistory } from 'react-router-dom';
+import { useIntl } from 'react-intl';
+import Fab from '@material-ui/core/Fab';
+import React from 'react';
+import clsx from 'clsx';
+const useStyles = makeStyles(styles);
+
+export const Wallets: React.FunctionComponent<IWalletsProps> = props => {
+	const classes = useStyles();
+	const history = useHistory();
+	const { formatMessage } = useIntl();
+	const [showDialog, setShowDialog] = React.useState<boolean>(false);
+	React.useEffect(() => {
+		const params: any = props.location.state;
+		const showDialog = params && params.showDialog ? params.showDialog : false;
+		setShowDialog(showDialog);
+	}, [props.location.state]);
+
+	const handleCreateWallet = (event: React.MouseEvent<HTMLElement>) => {
+		history.push('/wallets/create-wallet');
+	};
+
+	const handleAddFunds = (event: React.MouseEvent<HTMLElement>) => {
+		history.push('/wallets/add-funds');
+	};
+
+	const handleTransfer = (event: React.MouseEvent<HTMLElement>) => {
+		history.push('/wallets/transfer');
+	};
+
+	const handleTemplate = (event: React.MouseEvent<HTMLElement>) => {
+		history.push('/wallets/template');
+	};
+
+	return (
+		<Page title={formatMessage({ id: 'wallets.title' })} titleSize="large" headerLink="Payment methods" noHorizontalContentPadding>
+			<Box className={classes.walletsLogoContainer}>
+				<Box className={classes.walletsLogo}>
+					<IconButton iconProps={{ iconName: 'trash', color: '#ffffff' }} className={classes.trashIcon} />
+					<Text className={classes.rulerWalletText}>{formatMessage({ id: 'wallets.logo_title' })}</Text>
+					<Text className={classes.rulerPriceText}>€ 110 = 250 Ruler</Text>
+					<Text className={classes.rulerNumberText}>0 Ruler</Text>
+				</Box>
+			</Box>
+			<Box className={classes.paymentTemplateContainer}>
+				<Text className={classes.paymentTemplateText}>{formatMessage({ id: 'wallets.payment_templates' })}</Text>
+				<Box className={classes.TemplateButtonsWrapper}>
+					<Fab aria-label="add" className={classes.addFabButton} onClick={handleTemplate}>
+						<Icon iconName="add-without-circle" primaryColor="white" secondaryColor="white" />
+					</Fab>
+					<Button
+						fullWidth={false}
+						className={clsx(classes.paymentTemplateButton, 'to-friend')}
+						compact
+						iconName="edit"
+						iconPosition="right"
+					>
+						{formatMessage({ id: 'wallets.to_my_friend' })}
+					</Button>
+					<Button fullWidth={false} className={clsx(classes.paymentTemplateButton, 'to-wife')} compact iconName="edit" iconPosition="right">
+						{formatMessage({ id: 'wallets.to_wife' })}
+					</Button>
+				</Box>
+			</Box>
+			<Box className={classes.footer}>
+				<Box className={classes.buttonWrapper}>
+					<LightGreenButton compact iconName="add" onClick={handleCreateWallet}>
+						{formatMessage({ id: 'wallets.add_wallet' })}
+					</LightGreenButton>
+					<LightGreenButton compact iconName="transfer" onClick={handleTransfer}>
+						{formatMessage({ id: 'wallets.transfer' })}
+					</LightGreenButton>
+				</Box>
+				<GreenButton compact iconName="wallet" onClick={handleAddFunds}>
+					{formatMessage({ id: 'wallets.add_funds' })}
+				</GreenButton>
+			</Box>
+			<Dialog
+				open={showDialog}
+				hasClose={true}
+				illustrationName="superman"
+				onClose={(): void => setShowDialog(false)}
+				aria-labelledby="form-dialog-title"
+				title={formatMessage({ id: 'wallets.create_wallet.dialog.title' })}
+			>
+				<Text className={classes.dialogContentText}>{formatMessage({ id: 'wallets.create_wallet.dialog.description' })}</Text>
+			</Dialog>
+		</Page>
+	);
+};
