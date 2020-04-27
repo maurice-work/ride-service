b9447e971e7f289139a45a7288f8e7e2accc571a
maurice-work <maurice.devwork@gmail.com>
2020-04-27 22:56:35 +0800

    add i18n feature in news page

--- src/pages/News/News.tsx
+++ src/pages/News/News.tsx
@@ -3,14 +3,16 @@ import { NewsItem } from './components';
 import { Page } from 'components';
 import { newsData } from './News.data';
 import { styles } from './News.styles';
+import { useIntl } from 'react-intl';
 import React from 'react';
 const useStyles = makeStyles(styles);
 
 export const News: React.FunctionComponent = () => {
 	const classes = useStyles();
+	const { formatMessage } = useIntl();
 
 	return (
-		<Page title="News" titleSize="large" noHorizontalContentPadding>
+		<Page title={formatMessage({ id: 'news.title' })} titleSize="large" noHorizontalContentPadding>
 			<List className={classes.articleList}>
 				{newsData.map((newsData, index) => (
 					<NewsItem key={index} {...newsData} />
