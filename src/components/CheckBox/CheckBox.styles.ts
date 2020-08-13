f886ce540eded830a46dae5c889a0eb3914251bf
maurice-work <maurice.devwork@gmail.com>
2020-08-13 05:55:03 +0200

    Add Credit Card

--- src/components/CheckBox/CheckBox.styles.ts
+++ src/components/CheckBox/CheckBox.styles.ts
@@ -7,24 +7,30 @@ const root: CreateCSSProperties = {
 			fontWeight: 600,
 			lineHeight: 1.67
 		}),
-		color: (props: any) => (props.checked ? '#181c19' : 'rgba(28, 24, 25, 0.5)')
+		color: (props: any) =>
+			props.checked ? (props.isDarkMode ? '#ffffff' : '#181c19') : props.isDarkMode ? 'rgba(255, 255, 255, 0.5)' : 'rgba(28, 24, 25, 0.5)'
 	}
 };
 
-const checkBox: CSSProperties = {
+const checkBox: CreateCSSProperties = {
 	'&:hover': {
 		backgroundColor: 'transparent'
 	},
-	color: 'rgba(28, 24, 25, 0.5)',
+	color: (props: any) => (props.isDarkMode ? 'rgba(255, 255, 255, 0.5)' : 'rgba(28, 24, 25, 0.5)'),
 	'&$checked': {
 		color: '#181c19'
+	},
+	'&$checkedDark': {
+		color: '#ffffff'
 	}
 };
 
 const checked: CSSProperties = {};
 
+const checkedDark: CSSProperties = {};
 export const styles = createStyles({
 	root,
 	checkBox,
-	checked
+	checked,
+	checkedDark
 });
--- src/components/CheckBox/CheckBox.tsx
+++ src/components/CheckBox/CheckBox.tsx
@@ -1,3 +1,4 @@
+import { AppContext } from 'providers/State';
 import { FormControlLabel, Checkbox as MuiCheckbox, makeStyles } from '@material-ui/core';
 import { ICheckBoxProps } from './CheckBox.types';
 import { Icon } from 'components';
@@ -7,14 +8,19 @@ import React from 'react';
 const useStyles = makeStyles(styles);
 
 export const Checkbox: React.FunctionComponent<ICheckBoxProps> = ({ checked, label = '', onValueChange }) => {
-	const classes = useStyles({ checked });
+	const {
+		state: {
+			settings: { isDarkMode }
+		}
+	} = React.useContext(AppContext);
+	const classes = useStyles({ checked, isDarkMode: isDarkMode });
 
 	return (
 		<FormControlLabel
 			className={classes.root}
 			control={
 				<MuiCheckbox
-					classes={{ root: classes.checkBox, checked: classes.checked }}
+					classes={{ root: classes.checkBox, checked: isDarkMode ? classes.checkedDark : classes.checked }}
 					checkedIcon={<Icon iconName="save" />}
 					icon={<Icon iconName="save-blank" />}
 					onChange={onValueChange}
