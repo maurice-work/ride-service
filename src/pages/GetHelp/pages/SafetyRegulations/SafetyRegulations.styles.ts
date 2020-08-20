21aa2f33e1acb40b74a90a4fa818cdccc7e1cca9
maurice-work <maurice.devwork@gmail.com>
2020-08-20 16:00:25 +0200

    #89 scooter page content text color

--- src/pages/GetHelp/pages/SafetyRegulations/SafetyRegulations.styles.ts
+++ src/pages/GetHelp/pages/SafetyRegulations/SafetyRegulations.styles.ts
@@ -1,4 +1,4 @@
-import { CSSProperties, createStyles } from '@material-ui/styles';
+import { CSSProperties, CreateCSSProperties, createStyles } from '@material-ui/styles';
 import { DEFAULT_FONT_FAMILY, pxToRem } from 'styles';
 
 const list: CSSProperties = {
@@ -6,26 +6,40 @@ const list: CSSProperties = {
 };
 
 const listItem: CSSProperties = {
-	padding: `${pxToRem(11)} 0`,
+	padding: `${pxToRem(11)} 0`
+};
+
+const text: CreateCSSProperties = {
+	lineHeight: 1.67,
+	fontWeight: 600,
+	fontSize: pxToRem(15),
+	fontFamily: DEFAULT_FONT_FAMILY,
 	color: '#00b559'
 };
 
-const text: CSSProperties = {
+const content: CreateCSSProperties = {
+	opacity: 0.5,
+	lineHeight: 1.67,
+	fontWeight: 600,
+	fontSize: pxToRem(15),
+	fontFamily: DEFAULT_FONT_FAMILY,
+	color: (props: any) => (props.isDarkMode ? 'rgba(255, 255, 255)' : 'rgba(24, 28, 25)')
+};
+
+const subTitle: CSSProperties = {
+	margin: `${pxToRem(15)} 0 ${pxToRem(5)} 0`,
 	lineHeight: 1.67,
 	fontWeight: 600,
 	fontSize: pxToRem(15),
 	fontFamily: DEFAULT_FONT_FAMILY
 };
 
-const content: CSSProperties = {
-	opacity: 0.5
+const container: CreateCSSProperties = {
+	backgroundColor: (props: any) => (props.isDarkMode ? '#181c19' : '#fff'),
+	flex: 1
 };
-
-const subTitle: CSSProperties = {
-	margin: `${pxToRem(15)} 0 ${pxToRem(5)} 0`
-};
-
 export const styles = createStyles({
+	container,
 	list,
 	listItem,
 	text,
--- src/pages/GetHelp/pages/SafetyRegulations/SafetyScooter.tsx
+++ src/pages/GetHelp/pages/SafetyRegulations/SafetyScooter.tsx
@@ -1,5 +1,6 @@
+import { AppContext } from 'providers/State';
 import { Page, Text } from 'components';
-import { Paper, makeStyles } from '@material-ui/core';
+import { Paper, Typography, makeStyles } from '@material-ui/core';
 import { styles } from './SafetyRegulations.styles';
 import clsx from 'clsx';
 
@@ -8,31 +9,32 @@ import React from 'react';
 const useStyles = makeStyles(styles);
 
 export const SafetyScooter: React.FunctionComponent = () => {
-	const classes = useStyles();
+	const { state } = React.useContext(AppContext);
+	const classes = useStyles({ isDarkMode: state.settings.isDarkMode });
 
 	return (
 		<Page title="Scooter" titleSize="medium">
-			{/* <Paper elevation={0}> */}
-			<Text className={clsx(classes.subTitle, classes.text)}>Your safety is important to us</Text>
-			<Text className={clsx(classes.content, classes.text)} black>
-				Always follow the following rules while you are looking to ride via scooter.
-				<br />
-				<br />
-				1. Do a pre-ride check: Accelerator, Brake and Handle
-				<br />
-				2. Always follow helmet laws
-				<br />
-				3. Park properly at the designated Parking sites.
-				<br />
-				4. Apply brakes to slow down or stop
-				<br />
-				5. Take care of road obstructions
-				<br />
-				6. Always follow traffic rules
-				<br />
-				7. Do not use your phone while riding
-			</Text>
-			{/* </Paper> */}
+			<Text className={classes.subTitle}>Your safety is important to us</Text>
+			<Paper elevation={0} className={classes.container}>
+				<Typography className={classes.content}>
+					Always follow the following rules while you are looking to ride via scooter.
+					<br />
+					<br />
+					1. Do a pre-ride check: Accelerator, Brake and Handle
+					<br />
+					2. Always follow helmet laws
+					<br />
+					3. Park properly at the designated Parking sites.
+					<br />
+					4. Apply brakes to slow down or stop
+					<br />
+					5. Take care of road obstructions
+					<br />
+					6. Always follow traffic rules
+					<br />
+					7. Do not use your phone while riding
+				</Typography>
+			</Paper>
 		</Page>
 	);
 };
